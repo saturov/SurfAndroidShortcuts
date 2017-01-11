35de3a941f864d4c2c3439765fe775c1aac72650
saturov <saturov@surfstudio.ru>
2017-01-11 09:09:37 +0300

    Missing files

---
+++ app/src/main/java/ru/surf/android_shortcut_sample/FirstActivity.java
@@ -0,0 +1,13 @@
+package ru.surf.android_shortcut_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
