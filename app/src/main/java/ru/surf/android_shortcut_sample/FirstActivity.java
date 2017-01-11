b181ab8ca8f467d92ae7bcf8c700368bdf6527eb
saturov <saturov@surfstudio.ru>
2017-01-11 14:15:29 +0300

    Dynamic shortcuts

--- app/src/main/java/ru/surf/android_shortcut_sample/FirstActivity.java
+++ app/src/main/java/ru/surf/android_shortcut_sample/FirstActivity.java
@@ -1,7 +1,12 @@
 package ru.surf.android_shortcut_sample;
 
 import android.app.Activity;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
+
+import java.util.Arrays;
 
 public class FirstActivity extends Activity {
 
@@ -9,5 +14,17 @@ public class FirstActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
+
+        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
+                .setShortLabel("Dynamic")
+                .setLongLabel("Dynamic shortcut")
+                .setIcon(Icon.createWithResource(this, R.drawable.ic_shortcut))
+                .setIntent(SecondActivity.getIntent(this))
+                .build();
+
+        shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
+
     }
 }
--- app/src/main/java/ru/surf/android_shortcut_sample/SecondActivity.java
+++ app/src/main/java/ru/surf/android_shortcut_sample/SecondActivity.java
@@ -1,9 +1,15 @@
 package ru.surf.android_shortcut_sample;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends Activity {
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, SecondActivity.class).setAction(Intent.ACTION_VIEW);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
